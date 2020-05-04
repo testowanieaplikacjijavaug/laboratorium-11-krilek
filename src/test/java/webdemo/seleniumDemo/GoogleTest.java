bc75940f78e3e049111e20d2aa5026b92763ce35
krilek <krilek@gmail.com>
2020-05-04 11:52:05 +0200

    Zad2

--- src/test/java/webdemo/seleniumDemo/GoogleTest.java
+++ src/test/java/webdemo/seleniumDemo/GoogleTest.java
@@ -4,40 +4,44 @@ import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.seljup.Arguments;
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+@ExtendWith(SeleniumExtension.class)
 public class GoogleTest {
 	
-	private static WebDriver driver;
+	private WebDriver driver;
+	public GoogleTest(@Arguments("--headless") FirefoxDriver driver){
+		this.driver = driver;
+		this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-	@BeforeAll
-	public static void setUpDriver(){
-		System.setProperty("webdriver.gecko.driver", "resources/geckodriver" + (System.getProperty("os.name").toLowerCase().contains("win") ? ".exe" : "" ));
-        FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(true);
-        options.addPreference("intl.accept_languages", "en");
-		driver = new FirefoxDriver(options);
-		// Implicity wait -> max czas na znalezienie elementu na stronie
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-
 	@BeforeEach
 	public void setUp() throws Exception {
 		driver.get("https://duckduckgo.com/");
 	}
 
-	@AfterAll
-	public static void tearDown() throws Exception {
-		driver.quit();
+	public boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
 	}
 
 	@Test
@@ -50,7 +54,26 @@ public class GoogleTest {
 		String source = driver.getPageSource();
 		assertTrue(source.contains("DuckDuckGo"));
 	}
-	
+	@Test
+	public void checkSearchInputById(){
+		assertTrue(isElementPresent(By.id("search_form_input_homepage")));
+	}
+
+	@Test
+	public void checkSearchButtonByCss(){
+		assertTrue(isElementPresent(By.cssSelector("#search_button_homepage")));
+	}
+
+	@Test
+	public void checkTitleByClassName(){
+		assertTrue(isElementPresent(By.className("badge-link__title")));
+	}
+
+	@Test
+	public void checkSearchButtonByXPath(){
+		assertTrue(isElementPresent(By.xpath("//input[@type='submit']")));
+	}
+
 	@Test
 	public void testClick(){
 		driver.findElement(By.id("search_form_input_homepage")).sendKeys("Mateusz Miotk");
